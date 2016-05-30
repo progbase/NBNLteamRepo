1e371b04ba4dc356daa6deedbbc0e5863ee462b1
OlgaYashan <olga.yashan@gmail.com>
2016-05-30 11:54:01 +0300

    time_t2char

--- main.c
+++ main.c
@@ -1,19 +1,22 @@
 #include <time.h>
+#include <string.h>
+#include <stdio.h>
 
 char*  time_to_string(time_t time, char * buffer);
 int main ()
 {
-time_t rawtime;
-char * buffer;
- buffer = time_to_string(rawtime,buffer);
-printf("%s", buffer);
-  return 0;
+    time_t rawtime;
+    rawtime = time ( NULL );
+    char buffer[1000];
+    strcpy(buffer ,time_to_string(rawtime,buffer));
+    printf("%s", buffer);
+    return 0;
 }
 
-char*  time_to_string(time_t t, char * buffer){
-
-  time_t rawtime = t;
-  time ( &rawtime );
-  buffer = ctime (&rawtime);
-  return buffer;
+char*  time_to_string(time_t t, char * buffer)
+{
+    struct tm *gm;
+    gm = localtime(&t);
+    sprintf(buffer, "%d/%d/%d   %d.%d" ,gm->tm_year+1900, gm->tm_mon+1,gm->tm_mday,gm->tm_hour,gm->tm_min);
+    return buffer;
 }
